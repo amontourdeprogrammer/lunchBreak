a19f315c72083e451cc81f567a8ff3b4da6e894d
Yannick Francois <yaf@elsif.fr>
2017-05-17 11:31:03 +0200

    Show text with an update

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,33 +1,33 @@
 var socket = io();
+var game = {};
 
 window.onload = function() {
-
   socket.emit('hello','Hello world');
   socket.on('hello', function (message) {
     console.log(message);
   });
-
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '',
-      { preload: preload, create: create });
-
-  function preload () {
-    game.load.image('logo', 'images/logo.png');
-    game.load.spritesheet('ressources', 'images/ressources.png', 228, 228);
-  }
-
-  function create (){
-    game.stage.backgroundColor = '#D9F0F0';
-    //game.add.image(400, 300, 'ressources').anchor.setTo(0.5, 0.5);
-    placeOne(100, 100, 1)
-  }
-
-  function placeOne(x, y, frame) {
-    var ressource = game.add.sprite(
-        x,
-        y,
-        'ressources',
-        frame);
-    ressource.anchor.setTo(0.5, 0.5);
-    ressource.scale.setTo(0.5, 0.5);
-  }
+  game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 };
+
+function preload () {
+  game.load.image('logo', 'images/logo.png');
+  game.load.spritesheet('ressources', 'images/ressources.png', 228, 228);
+}
+
+function create (){
+  game.stage.backgroundColor = '#D9F0F0';
+  //game.add.image(400, 300, 'ressources').anchor.setTo(0.5, 0.5);
+  text = game.add.text(32, 32, "no user" , 16);
+  placeOne(100, 100, 1)
+}
+
+function update () {
+  text.text = new Date();
+}
+
+function placeOne(x, y, frame) {
+  var ressource = game.add.sprite(x, y, 'ressources', frame);
+  ressource.anchor.setTo(0.5, 0.5);
+  ressource.scale.setTo(0.5, 0.5);
+}
+
