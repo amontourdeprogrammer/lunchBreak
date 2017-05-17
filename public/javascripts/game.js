bf1b7f0715021064c9c250101018cf314d940392
Lorena Rangel <rangel.loreen@gmail.com>
2017-05-17 15:46:50 +0200

    Character moves with keyboard keys

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,6 +2,8 @@ var socket = io();
 var game = {};
 var gameState = {players: 1};
 
+var player;
+
 window.onload = function() {
   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 };
@@ -20,10 +22,12 @@ function create (){
   text = game.add.text(64, 362, "no user" , 16);
   placeOne(100, 100, 1);
   placeCharacter(400,100);
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update () {
   text.text = "Players : " + gameState.players;
+  moveCharacter(); 
 }
 
 function placeOne(x, y, frame) {
@@ -33,7 +37,32 @@ function placeOne(x, y, frame) {
 }
 
 function placeCharacter(x, y) {
-  var player = game.add.sprite(x, y, 'player');
+  player = game.add.sprite(x, y, 'player');
   player.anchor.setTo(0.5, 0.5);
   player.scale.setTo(0.2, 0.2);
+
+  game.physics.arcade.enable(player);
+}
+
+function moveCharacter() {
+  player.body.velocity.x = 0;
+  player.body.velocity.y = 0;
+
+  if (cursors.left.isDown)
+  {
+      player.body.velocity.x = -200;
+  }
+  else if (cursors.right.isDown)
+  {
+      player.body.velocity.x = 200;
+  }
+
+  if (cursors.up.isDown)
+  {
+      player.body.velocity.y = -200;
+  }
+  else if (cursors.down.isDown)
+  {
+      player.body.velocity.y = 200;
+  }
 }
