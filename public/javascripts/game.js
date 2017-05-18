2acd7ffa1373b2dc15a29639c1011299a0fbc339
meo84 <oopsiday@gmail.com>
2017-05-18 21:21:17 +0200

    place walls on gameset
    - when loading the game, walls are displayed on the background of the
      set
    - but ressources can be placed on top of the walls: we'll prevent this
      behaviour in the next commit!

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -22,25 +22,9 @@ function preload () {
 }
 
 function create (){
-  map = game.add.tilemap();
-  map.addTilesetImage('ressources');
-  layer_background = map.create('space_backgrounds', 40, 30, 32, 32);
 
-  for (var i = 0; i < 16; i++){
-    var x = game.rnd.integerInRange(0, 24)
-    var y = game.rnd.integerInRange(0, 18)
-    map.putTile(i, x ,y, layer_background);
-
-  map.addTilesetImage('tiles');
-  layer_walls = map.create('space_walls', 40, 30, 32, 32);
-
-  drawWall(10, 10, 'vertical', 3, layer_walls);
-  drawWall(3, 0, 'horizontal', 7, layer_walls);
-
-  game.stage.backgroundColor = '#D9F0F0';
-  text = game.add.text(64, 362, "no user" , 16);
-  placeOne(100, 100, 1)
-}
+  placeWalls(game);
+  placeRessources(game);
 
   game.stage.backgroundColor = '#D1EDEC';
   text = game.add.text(64, 362, "no user" , 16);
@@ -51,7 +35,7 @@ function create (){
   placeCharacter(x,y);
   userID = Math.random() * 1000;
   socket.emit("new user",{userObj:userID, xObj:x, yObj:y});
- }
+}
 
 function update () {
   text.text = "Players : " + gameState.players;
@@ -72,26 +56,59 @@ function moveCharacter() {
 
   if (cursors.left.isDown)
   {
-      player.body.velocity.x = -200;
+    player.body.velocity.x = -200;
   }
   else if (cursors.right.isDown)
   {
-      player.body.velocity.x = 200;
+    player.body.velocity.x = 200;
   }
 
   if (cursors.up.isDown)
   {
-      player.body.velocity.y = -200;
+    player.body.velocity.y = -200;
   }
   else if (cursors.down.isDown)
   {
-      player.body.velocity.y = 200;
+    player.body.velocity.y = 200;
   }
 }
 
-function drawWall(x, y, direction, tilelength, layer) {
-  for(i=0; i<tilelength; i++) {
-    if(direction==='horizontal') { map.putTile(0, x, y + i, layer); }
-    else if(direction==='vertical') { map.putTile(0, x + i, y, layer); }
+function placeWalls(game) {
+  wallsMap = game.add.tilemap();
+  wallsMap.addTilesetImage('tiles');
+
+  wallsLayer = wallsMap.create('walls', 40, 30, 32, 32);
+
+  placeWall(10, 10, 'vertical', 3, wallsLayer);
+  placeWall(3, 0, 'vertical', 7, wallsLayer);
+  placeWall(10, 9, 'horizontal', 4, wallsLayer);
+  placeWall(20, 5, 'horizontal', 5, wallsLayer);
+  placeWall(18, 13, 'vertical', 5, wallsLayer);
+  placeWall(15, 0, 'vertical', 8, wallsLayer);
+  placeWall(0, 14, 'horizontal', 6, wallsLayer);
+}
+
+function placeWall(x, y, direction, tileLength, layer) {
+  if(direction==='vertical') {
+    for(i=0; i<tileLength; i++) {
+      wallsMap.putTile(0, x, y + i, layer);
+    }
+  } else if(direction==='horizontal') {
+    for(i=0; i<tileLength; i++) {
+      wallsMap.putTile(0, x + i, y, layer);
+    }
+  }
+}
+
+function placeRessources(game) {
+  ressourcesMap = game.add.tilemap();
+  ressourcesMap.addTilesetImage('ressources');
+
+  ressourcesLayer = ressourcesMap.create('space_backgrounds', 40, 30, 32, 32);
+
+  for (var i = 0; i < 16; i++){
+    var x = game.rnd.integerInRange(0, 24)
+      var y = game.rnd.integerInRange(0, 18)
+      ressourcesMap.putTile(i, x ,y, ressourcesLayer);
   }
 }
