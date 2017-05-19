967934ed93dd51c9da713f646db9f5882c97cdb3
Votre nom ou pseudo <Votre@email.comEliPamp>
2017-05-19 14:47:05 +0200

    empêche le perso de sortir du cadre

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -75,24 +75,41 @@ function moveCharacter() {
   player.body.velocity.x = 0;
   player.body.velocity.y = 0;
 
-  if (cursors.left.isDown)
-  {
-    player.body.velocity.x = -200;
+  if (cursors.left.isDown) { 
+    if (player.body.x > 0) {
+      player.body.velocity.x = -200;
+    } else {
+      player.body.velocity.x = 0;
+    }
   }
-  else if (cursors.right.isDown)
-  {
-    player.body.velocity.x = 200;
+  else if (cursors.right.isDown) {
+    if (player.body.x < max_x-80) {
+      player.body.velocity.x = 200;
+    } else {
+      player.body.velocity.x = 0;
+    }
   }
 
-  if (cursors.up.isDown)
-  {
-    player.body.velocity.y = -200;
+  if (cursors.up.isDown) {
+    if (player.body.y > 0){ 
+      player.body.velocity.y = -200;
+    } else {
+      player.body.velocity.y = 0;
+    }
   }
-  else if (cursors.down.isDown)
-  {
-    player.body.velocity.y = 200;
+  else if (cursors.down.isDown) {
+    if (player.body.y < max_y-80){
+      player.body.velocity.y = 200;
+    } else {
+      player.body.velocity.y = 0;
+    }
   }
+
 }
+ 
+
+
+
 
 function placeCharacters(){
   listOfCharac = {hello:[334, 267]}
